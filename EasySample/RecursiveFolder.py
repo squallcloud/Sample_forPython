29e372d7814951e45e9044c3d56873f06c40bd98
squallcloud <squallcloudzidane@gmail.com>
2018-10-05 10:38:33 +0900

    feat: 簡単なスクリプト追加

---
+++ EasySample/RecursiveFolder.py
@@ -0,0 +1,24 @@
+﻿# 再帰的にファイル・ディレクトリを探索する
+from pathlib import Path
+
+rootPath = 'C:/Users/cloud/Desktop/Multipurpose/Programming'
+
+p = Path(rootPath)
+
+# list(p)
+
+# dir_A直下のファイルとディレクトリを取得
+# Path.glob(pattern)はジェネレータを返す。結果を明示するためlist化しているが、普段は不要。
+# l = list(p.glob("*"))
+# for x in l:
+#   print(x)
+
+# ファイル名の条件指定
+# l = list(p.glob("*.txt"))
+# for x in l:
+#   print(x)
+
+# 再帰的な検索
+l = list(p.glob("**/*"))
+for x in l:
+  print(x)
