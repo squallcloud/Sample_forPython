e6c368dd2391e8f925a98586130aaf03622f8260
squallcloud <squallcloudzidane@gmail.com>
2018-10-15 00:16:35 +0900

    feat: エクセル操作のスクリプトを追加

---
+++ EasySample/ExcelTest.py
@@ -0,0 +1,22 @@
+﻿import openpyxl
+
+xl_path = "sample.xlsx"
+
+wb = openpyxl.load_workbook(xl_path)
+for sheet_name in wb.sheetnames:
+  print(sheet_name)
+  sheet = wb[sheet_name]
+  print(sheet.max_column)
+  print(sheet.max_row)
+
+  for row_idx in range(sheet.max_row):
+    for column_idx in range(sheet.max_column):
+      cell = sheet.cell(row=row_idx+1, column=column_idx+1)
+      value = cell.value
+      if type(value) is str:
+        repalce_value = value.replace("hoge", "foo")
+        cell.value = repalce_value
+        cell.value = 10
+
+wb.save(xl_path)
+wb.close()
